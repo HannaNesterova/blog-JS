437bab9ddb8ac1ee2114a7b3e11dc7855aca1555
Hanna Nesterova <nesterova.hanna@gmail.com>
2023-01-05 17:30:00 +0100

    Change css color and decoration for back and li

--- js/script.js
+++ js/script.js
@@ -28,8 +28,8 @@ function titleClickHandler(event){
     targetArticle.classList.add('active');
     }
 
+    
   const links = document.querySelectorAll('.titles a');
-  
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
   }
