a7d13ad9a69bd3afde51d906c70c3ece2a73f554
Hanna Nesterova <nesterova.hanna@gmail.com>
2023-01-05 13:45:27 +0100

    Add active function fot clicked links

--- js/script.js
+++ js/script.js
@@ -1,34 +1,32 @@
 'use strict';
 
-// const links = document.querySelectorAll('.titles a');
-
-// for(let link of links){
-//     console.log(link);
-//     link.addEventListener('click', );
-// }
 function titleClickHandler(event){
-
-    /* remove class 'active' from all article links  */
+    const clickedElement = this;
+   
+    /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
     for(let activeLink of activeLinks){
         activeLink.classList.remove('active');
     }
-  /* remove class 'active' from all articles */
+  /* [DONE] remove class 'active' from all articles */
     const activeArticles=document.querySelectorAll('.post.active');
     for(let activeArticle of activeArticles){
         activeArticle.classList.remove('active');
     }
     
-  /* add class 'active' to the clicked link */
+  /*[DONE] add class 'active' to the clicked link */
+  const addActiveLinks = clickedElement;
+  addActiveLinks.classList.add('active');
+  console.log('clickedElement:', clickedElement);
+  }
 
 
- 
   /* get 'href' attribute from the clicked link */
 
   /* find the correct article using the selector (value of 'href' attribute) */
 
   /* add class 'active' to the correct article */
-}
+
   
   const links = document.querySelectorAll('.titles a');
   
